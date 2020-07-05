6007a32b8c1c78c0fd3ea3fc1847e3a134e0ad1e
Lucas Amaro <lucasrla@users.noreply.github.com>
2020-07-05 16:46:15 -0300

    Publish container :8080 to host :80, bump docker-compose version to 1.26.2

--- lightsail-startup-script.sh
+++ lightsail-startup-script.sh
@@ -6,11 +6,12 @@
 # install latest version of docker the lazy way
 curl -sSL https://get.docker.com | sh
 
-# make it so you don't need to sudo to run docker commands
+# add the default ubuntu user to the docker group
+# so that we don't need to sudo to run docker commands
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
